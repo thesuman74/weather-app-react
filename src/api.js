1ac69054b2593dcfc48b30362822e357a403def5
Suman Adhikari <58351346+thesuman74@users.noreply.github.com>
2024-05-05 18:57:33 +0545

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ export const geoOptionAPi = {
 };
 
 export const GEO_API_URL = "https://api.geoapify.com/v1/geocode";
-export const GEO_API_KEY = "4b68dd20c4b84c74ab7db31ebc8425fd";
+export const GEO_API_KEY = "#######################";
 export const WEATHER_API_URL = "https://api.open-meteo.com/v1/forecast?";
 // https://api.open-meteo.com/v1/bom?latitude=52.52&longitude=13.41&hourly=temperature_2m
 // https://api.open-meteo.com/v1/forecast?latitude=28.2669&longitude=83.9685&current=temperature_2m,relative_humidity_2m,precipitation,rain,weather_code,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m&timezone=auto
